d628d99c3112c1e625084f297152fccd3dca8efe
giant-paw <giantpawbusiness@gmail.com>
2024-12-21 09:20:21 +0700

    membuat initializer untu dosen view model dan home dosen nya

--- app/src/main/java/com/example/a18_december/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a18_december/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,28 @@
 package com.example.a18_december.viewmodel
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a18_december.UmyApp
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            DosenViewModel(
+                UmyApp().containerApp.repositoryDsn
+            )
+        }
+
+        initializer {
+            HomeDsnViewModel(
+                umyApp().containerApp.repositoryDsn
+            )
+        }
+
+    }
+}
+
+fun CreationExtras.umyApp(): UmyApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as UmyApp)
