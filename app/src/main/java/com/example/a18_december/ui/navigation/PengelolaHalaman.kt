4cc7e634db3ddf993fd5309d48e345bc352b3d46
giant-paw <giantpawbusiness@gmail.com>
2024-12-21 10:56:20 +0700

    mengatur insertDosen di view InsertDsnView dan mengatur PengelolaHalaman

--- app/src/main/java/com/example/a18_december/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/a18_december/ui/navigation/PengelolaHalaman.kt
@@ -1,2 +1,48 @@
 package com.example.a18_december.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.a18_december.view.dosen.DestinasiInsert
+import com.example.a18_december.view.dosen.HomeDsnView
+import com.example.a18_december.view.dosen.InsertDsnView
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = AlamatNavigasi.DestinasiHome.route) {
+        composable(
+            route = AlamatNavigasi.DestinasiHome.route
+        ){
+            HomeDsnView(
+                onAddDsn = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                modifier = modifier
+            )
+        }
+
+        composable(
+            route = DestinasiInsert.route
+        ){
+            InsertDsnView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/a18_december/view/dosen/InsertDsnView.kt
+++ app/src/main/java/com/example/a18_december/view/dosen/InsertDsnView.kt
@@ -1,27 +1,101 @@
 package com.example.a18_december.view.dosen
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a18_december.data.entity.Dosen
+import com.example.a18_december.ui.customwidget.CustomTopAppBar
+import com.example.a18_december.ui.navigation.AlamatNavigasi
 import com.example.a18_december.viewmodel.DosenEvent
+import com.example.a18_december.viewmodel.DosenViewModel
 import com.example.a18_december.viewmodel.DsnUIState
 import com.example.a18_december.viewmodel.FormErrorState
+import com.example.a18_december.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
+object DestinasiInsert : AlamatNavigasi {
+    override val route: String = "insert_mhs"
+}
+
+
+@SuppressLint("RememberReturnType", "UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun InsertDsnView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DosenViewModel = viewModel(factory = PenyediaViewModel.Factory)  // Inisialisasi ViewModel
+) {
+    val uiState = viewModel.uiState         // Ambil UI State dari ViewModel
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    val coroutineScope = rememberCoroutineScope()
+
+    // Observasi Perubahan SnackBar effect
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)     // Tampilkan Snackbar
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) } //Tempelkan snackbar di Scaffold
+    ) { padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ){
+            CustomTopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Dosen"
+            )
+
+            // ISI BODY
+            InsertBodyDsn(
+                uiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent)  // Update State di ViewModel
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        viewModel.saveData()    // Simpan Data
+                    }
+                    onNavigate()
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun InsertBodyDsn(
