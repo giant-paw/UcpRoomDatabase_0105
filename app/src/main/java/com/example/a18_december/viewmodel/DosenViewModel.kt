9e4eed56a0511df4904e4c7ebf63ce2a296a4095
giant-paw <giantpawbusiness@gmail.com>
2024-12-21 08:22:39 +0700

    memperbaiki viewmodel untuk dosen dan membuat class DosenViewModel

--- app/src/main/java/com/example/a18_december/viewmodel/DosenViewModel.kt
+++ app/src/main/java/com/example/a18_december/viewmodel/DosenViewModel.kt
@@ -3,32 +3,65 @@ package com.example.a18_december.viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.a18_december.data.entity.Dosen
 import androidx.lifecycle.viewModelScope
+import com.example.a18_december.repository.RepositoryDsn
+import kotlinx.coroutines.launch
 
 var uiState by mutableStateOf(DsnUIState())
 
+class DosenViewModel (private val repositoryDsn: RepositoryDsn) : ViewModel() {
 
+    var uiState by mutableStateOf(DsnUIState())
 
-private fun validateFields(): Boolean {
-    val event = uiState.dosenEvent
-    val errorState = FormErrorState(
-        nidn = if (event.nidn.isNotEmpty()) null else "NIDN tidak boleh kosong",
-        nama = if (event.nidn.isNotEmpty()) null else "Nama tidak boleh kosong",
-        jenisKelamin = if (event.nidn.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
-    )
-    uiState = uiState.copy(isEntryValid = errorState)
-    return errorState.isValid()
-}
+    // memperbarui state berdasarkan input penggguna
 
-fun saveData() {
-    val currentEvent = uiState.dosenEvent
-    if (validateFields()) {
-        viewModelScope.launch {
-            try {
+    fun updateState(dosenEvent: DosenEvent) {
+        uiState = uiState.copy(
+            dosenEvent = dosenEvent
+        )
+    }
 
+    // Validasi data input pengguna
+    private fun validateFields(): Boolean {
+        val event = uiState.dosenEvent
+        val errorState = FormErrorState(
+            nidn = if (event.nidn.isNotEmpty()) null else "NIDN tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun saveData() {
+        val currentEvent = uiState.dosenEvent
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryDsn.insertDsn(currentEvent.toDosenEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data Berhasil Dismpan",
+                        dosenEvent = DosenEvent(), // Reset input form
+                        isEntryValid = FormErrorState() // Reset Error State
+                    )
+                } catch (e: Exception) {
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data Gagal Disimpan"
+                    )
+                }
             }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input Tidak Valid. Periksa Kembali Data Anda!!"
+            )
         }
+    }
+
+    // Reset Pesan SnackBar Setelah Ditampilkan
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(snackBarMessage = null)
     }
 }
 
